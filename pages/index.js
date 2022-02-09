03e1ce01e3999b2e70d753f86ec50dcb77d43378
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-09 10:15:22 +0000

    Add example of getServerSideProps on index

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,17 @@ import styles from "@styles/Layout.module.css";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 
-export default function Home() {
+export async function getServerSideProps(context) {
+  const name = context.query.name || "stranger";
+
+  return {
+    props: {
+      name,
+    },
+  };
+}
+
+export default function Home({ name }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,14 +26,34 @@ export default function Home() {
       <Header />
 
       <main className={styles.main}>
-        <h1>Welcome! Browse Pokemon and check the price of Bitcoin.</h1>
-        <Link href="/pokemon">
-          <a>View all Pokemon</a>
-        </Link>
-
+        <h1>Welcome {name}!</h1>
+        <h2>Browse Pokemon and check the price of Bitcoin.</h2>
+        <p>
+          <Link href="/pokemon">
+            <a>View all Pokemon</a>
+          </Link>
+        </p>
+        <p>
+          <Link href="/bitcoin">
+            <a>Check Bitcoin</a>
+          </Link>
+        </p>
         <div className={styles.explainer}>
           <p>
-            This is a static route. Find the file at <code>/pages/index.js</code>
+            This is a route that uses <code>getServerSideProps()</code> to show dynamic data at request time. Experiment
+            with adding <code>?name=YOUR_NAME</code> to the URL.
+          </p>
+          <p>
+            Find the file at <code>/pages/index.js</code>
+          </p>
+          <p>
+            Check out the{" "}
+            <a
+              href="https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props"
+              target="_blank"
+              rel="noreferrer">
+              Next.js docs for getServerSideProps().
+            </a>
           </p>
         </div>
       </main>
